14af73cce85647a5362aeec43ba71582a7fe0640
Edward Konovalov <edk@primexbt.ch>
2021-12-01 15:39:22 +0300

    refactoring

--- public/js/app.js
+++ public/js/app.js
@@ -7,21 +7,22 @@ const app = Vue.createApp({
             subject: null,
             langs: [],
             pages: [],
-            resolutions: [661, 480, 380],
+            defaultResolutions: [661, 480, 380],
             copied: false,
             filters: {
-                "all": () => true
+                all: () => true,
             },
-            selectedFilter: "all",
-            stickyControlPanel: JSON.parse(localStorage.getItem('stickyControlPanel')) || false
-        }
+            selectedFilter: 'all',
+            stickyControlPanel: JSON.parse(localStorage.getItem('stickyControlPanel')) || false,
+            oneResolution: JSON.parse(localStorage.getItem('oneResolution')) || false,
+        };
     },
     created() {
         this.fetchEmails();
-        window.addEventListener("keydown", (e) => {
+        window.addEventListener('keydown', (e) => {
             const { key } = e;
 
-            if (document.activeElement.classList.contains("form-select") || !["ArrowUp", "ArrowDown"].includes(key)) {
+            if (document.activeElement.classList.contains('form-select') || !['ArrowUp', 'ArrowDown'].includes(key)) {
                 return false;
             }
 
@@ -29,105 +30,106 @@ const app = Vue.createApp({
 
             const pages = this.pages.filter(this.filters[this.selectedFilter]);
             const currentPageIndex = pages.indexOf(this.page);
-            const newPageIndex = key === "ArrowUp" ? currentPageIndex - 1 : currentPageIndex + 1;
+            const newPageIndex = key === 'ArrowUp' ? currentPageIndex - 1 : currentPageIndex + 1;
             if (newPageIndex >= 0 && newPageIndex < pages.length) {
                 this.page = pages[newPageIndex];
             }
         });
     },
     beforeUpdate() {
-        this.updateTitle()
-        this.updateSubject()
-        this.updateIframeHeight()
-        this.copied = false
+        this.updateTitle();
+        this.updateSubject();
+        // this.updateIframeHeight();
+        this.copied = false;
     },
     computed: {
         filteredPages() {
-            return this.pages.filter(this.filters[this.selectedFilter])
+            return this.pages.filter(this.filters[this.selectedFilter]);
+        },
+        resolutions() {
+            return this.oneResolution ? [this.defaultResolutions[0]] : this.defaultResolutions;
         },
     },
     watch: {
         selectedFilter(newFilter) {
-            localStorage.setItem('selectedFilter', newFilter)
-            this.page = this.pages.filter(this.filters[this.selectedFilter]).shift()
+            localStorage.setItem('selectedFilter', newFilter);
+            const filter = this.filters[this.selectedFilter];
+            const filtred = this.pages.filter(filter);
+            this.page = filtred.includes(this.page) ? this.page : filtred.shift();
         },
         stickyControlPanel(value) {
-            localStorage.setItem('stickyControlPanel', value)
+            localStorage.setItem('stickyControlPanel', value);
         },
         page(newPage) {
-            localStorage.setItem('selectedPage', newPage)
+            localStorage.setItem('selectedPage', newPage);
         },
-        lang(newLang) {
-            localStorage.setItem('selectedLang', newLang)
-        }
+        oneResolution(value) {
+            localStorage.setItem('oneResolution', value);
+        },
     },
     methods: {
         getUrl(type) {
             return {
                 template: `./${this.lang}/${this.page}.html`,
-                subject: `./${this.lang}/${this.page}_subject.html`
+                subject: `./${this.lang}/${this.page}_subject.html`,
             }[type];
         },
         updateTitle() {
             const text = this.langs[this.lang] + ' - ' + this.page;
-            document.querySelector('title').innerText = text
+            document.querySelector('title').innerText = text;
         },
         copyEmailHtmlToClipboard() {
-            const iframe = document.querySelector('iframe.preview-iframe')
-            const content = iframe.contentWindow.document.documentElement.innerHTML
-            return navigator.clipboard.writeText(content)
-                .then(() => this.copied = true)
+            const iframe = document.querySelector('iframe.preview-iframe');
+            const content = iframe.contentWindow.document.documentElement.innerHTML;
+            return navigator.clipboard.writeText(content).then(() => (this.copied = true));
         },
         goToPostdrop(e) {
             e.preventDefault();
-            this.copyEmailHtmlToClipboard()
-                .then(() => window.open(e.target.dataset.href));
+            this.copyEmailHtmlToClipboard().then(() => window.open(e.target.dataset.href));
         },
-        openEmailInNewTab({target}) {
+        openEmailInNewTab({ target }) {
             window.open(target.dataset.href);
         },
         updateSubject() {
             fetch(this.getUrl('subject'))
-                .then(response => response.arrayBuffer())
-                .then(buffer => new TextDecoder("utf-16be").decode(buffer))
+                .then((response) => response.arrayBuffer())
+                .then((buffer) => new TextDecoder('utf-16be').decode(buffer))
                 .then((subject) => {
-                    this.subject = subject
-                })
+                    this.subject = subject;
+                });
         },
         updateIframeHeight() {
-            document.querySelectorAll('iframe')
-                .forEach((iframe) => {
-                    iframe.onload = function () {
-                        this.classList.remove('loading');
-                        this.style.height = this.contentDocument.body.scrollHeight + 'px';
-                    }
-                })
+            // document.querySelectorAll('iframe').forEach((iframe) => {
+            //     iframe.onload = function () {
+            //         this.style.height = this.contentDocument.body.scrollHeight + 'px';
+            //     };
+            // });
         },
         fetchEmails() {
-            this.fetchingEmails = true
+            this.fetchingEmails = true;
             return fetch('/emails')
-                .then(emails => emails.json())
-                .then(emails => {
-                    this.langs = emails.langs
-                    this.pages = emails.pages
-                    this.page = localStorage.getItem('selectedPage') || this.pages[0]
+                .then((emails) => emails.json())
+                .then((emails) => {
+                    this.langs = emails.langs;
+                    this.pages = emails.pages;
+                    this.page = localStorage.getItem('selectedPage') || this.pages[0];
 
                     this.filters = {
-                        "all": () => true,
-                        "marketing": (pageName) => pageName.includes(emails.project === 'PRIME' ? 'did-you-know' : 'marketing'),
-                        "service": (pageName) => !this.filters.marketing(pageName)
-                    }
+                        all: () => true,
+                        marketing: (pageName) =>
+                            pageName.includes(emails.project === 'PRIME' ? 'did-you-know' : 'marketing'),
+                        service: (pageName) => !this.filters.marketing(pageName),
+                    };
 
-                    this.selectedFilter = localStorage.getItem('selectedFilter') || Object.keys(this.filters).shift()
+                    this.selectedFilter = localStorage.getItem('selectedFilter') || Object.keys(this.filters).shift();
 
-                    const self = this
+                    const self = this;
                     setTimeout(() => {
-                        self.fetchingEmails = false
-                    }, 1000)
-
-                })
-        }
-    }
-})
+                        self.fetchingEmails = false;
+                    }, 1000);
+                });
+        },
+    },
+});
 
 app.mount('#app');
